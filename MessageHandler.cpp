06fd5de38b9668dbaebebb165e122eec150fcb5a
IlNevioIl <36001296+IlNevioIl@users.noreply.github.com>
2023-11-13 21:25:12 +0100

    Added function to print to Neighborhood console

---
+++ MessageHandler.cpp
@@ -0,0 +1,126 @@
+#include "../include/MessageHandler.h"
+
+//
+#include "../include/SystemWrapper.h"
+#include "../include/INIHandler.h"
+
+#include <stdint.h>
+#include <iostream>
+#include <cstdio>
+#include <time.h>
+
+namespace xUtilty
+{
+	void LocalPrint(const char* a_fmt, ...)
+	{
+		//
+		char buffer[1024];
+
+
+		va_list args;
+		va_start(args, a_fmt);
+		size_t length = SystemWrapper::vsprintf(buffer, a_fmt, args);
+		va_end(args);
+	}
+
+	void Notify(const char* a_fmt, ...)
+	{
+		//
+		NotifyBuffer Buffer;
+
+		va_list args;
+		va_start(args, a_fmt);
+		size_t length = SystemWrapper::vsprintf(Buffer.Message, a_fmt, args);
+		va_end(args);
+
+		Buffer.Type = NotifyType::NotificationRequest;
+		Buffer.unk3 = 0;
+		Buffer.UseIconImageUri = 1;
+		Buffer.TargetId = -1;
+
+		auto* INI = INIHandler::INIHandler::GetSingleton();
+		if (INI->GetINIOptions()->UseCustomIconURL)
+		{
+			strncpy(Buffer.Uri, INI->GetINIOptions()->Icon, sizeof(Buffer.Uri));
+		}
+		else
+		{
+			strncpy(Buffer.Uri, "https://www.akcpetinsurance.com/res/akc/images/icons/home/home_dog.png", sizeof(Buffer.Uri));
+		}
+
+#if defined (__ORBIS__) || defined(__OPENORBIS__)
+		SystemWrapper::sceKernelSendNotificationRequest(0, (char*)&Buffer, 3120, 0);
+		KernelPrintOut(Buffer.Message, "\n");
+
+#elif defined(__SWITCH__) || defined(PLATFORM_NX)
+		svcOutputDebugString(Buffer.Message, length);
+#elif defined(_WIN32) || defined(_WIN64)
+		printf(Buffer.Message);
+#endif
+	}
+
+	void KernelPrintOut(const char* a_fmt, ...)
+	{
+		//
+		int64_t s_time;
+		char MessageBuf[1024];
+
+		time(&s_time);
+		auto time = localtime(&s_time);
+		strftime(MessageBuf, 1024, "[%m/%d/%Y - %I:%M:%S%p] ", time);
+
+		// stub the size.
+		auto timelen = strlen(MessageBuf);
+
+		va_list args;
+		va_start(args, a_fmt);
+		size_t length = SystemWrapper::vsprintf(&MessageBuf[timelen], a_fmt, args);
+		va_end(args);
+
+#if defined (__ORBIS__) || defined(__OPENORBIS__)
+		SystemWrapper::sceKernelDebugOutText(0, MessageBuf);
+		SystemWrapper::sceKernelDebugOutText(0, "\n");
+#elif defined(__SWITCH__) || defined(PLATFORM_NX)
+		svcOutputDebugString(MessageBuf, length);
+#elif defined(_WIN32) || defined(_WIN64)
+		printf(MessageBuf);
+#endif
+	}
+
+	void CoredumpPrintOut(const char* a_fmt, ...)
+	{
+		//
+		int64_t s_time;
+		char MessageBuf[1024];
+
+		time(&s_time);
+		auto time = localtime(&s_time);
+		strftime(MessageBuf, 1024, "[%m/%d/%Y - %I:%M:%S%p] ", time);
+
+		// stub the size.
+		auto timelen = strlen(MessageBuf);
+
+		va_list args;
+		va_start(args, a_fmt);
+		size_t length = SystemWrapper::vsprintf(&MessageBuf[timelen], a_fmt, args);
+		va_end(args);
+
+#if defined (__ORBIS__) || defined(__OPENORBIS__)
+		SystemWrapper::sceCoredumpDebugTextOut(MessageBuf, length);
+		SystemWrapper::sceCoredumpDebugTextOut("\n", length);
+#elif defined(__SWITCH__) || defined(PLATFORM_NX)
+		svcOutputDebugString(MessageBuf, length);
+#elif defined(_WIN32) || defined(_WIN64)
+		printf(MessageBuf);
+#endif
+	}
+
+	void PrintNeighborhood(const char* a_fmt, ...) 
+	{
+		for (const char* msg = a_fmt; *msg != '\0'; ++msg) {
+			putchar(*msg);
+		}
+		fflush(stdout);
+	}
+
+}
---
+++ MessageHandler.h
@@ -0,0 +1,124 @@
+#pragma once
+
+// THINKING
+#include "../include/Macro.h"
+
+#include <stdint.h>
+#include <cstring>
+#include <stdio.h>
+#include <stdarg.h>
+
+// print the current code pos into the kernel log(debug)
+#ifndef PRINT_POS
+#define PRINT_POS xUtilty::KernelPrintOut("%s %s %d", __FUNCTION__, __FILE__, __LINE__);
+#define PRINT_POS_N xUtilty::Notify("%s %s %d", __FUNCTION__, __FILE__, __LINE__);
+#endif
+
+// print the current code pos into the kernel log and return(debug)
+#ifndef PRINT_POS_RETURN 
+#define PRINT_POS_RETURN xUtilty::KernelPrintOut("%s %s %d %s", __FUNCTION__, __FILE__, __LINE__, THINKING); return;
+#define PRINT_POS_RETURN_N xUtilty::Notify("%s %s %d %s", __FUNCTION__, __FILE__, __LINE__, THINKING); return;
+
+#endif
+
+// print the current code pos + custom fmt + string into the kernel log 
+#ifndef PRINT_FMT
+#define PRINT_FMT(fmt, ...) xUtilty::KernelPrintOut("%s %s %d %s " fmt, __FUNCTION__, __FILE__, __LINE__, THINKING, __VA_ARGS__);
+#define PRINT_FMT_N(fmt, ...) xUtilty::Notify("%s %s %d %s " fmt, __FUNCTION__, __FILE__, __LINE__, THINKING, __VA_ARGS__);
+#endif
+
+// print the current code pos + custom fmt + string into the kernel log and return T
+#ifndef PRINT_FMT_RETURN_T
+#define PRINT_FMT_RETURN_T(T, fmt, ...) xUtilty::KernelPrintOut("%s %s %d %s "  fmt, __FUNCTION__,__FILE__, __LINE__, THINKING, __VA_ARGS__); return T;
+#define PRINT_FMT_RETURN_T_N(T, fmt, ...) xUtilty::Notify("%s %s %d %s "  fmt, __FUNCTION__,__FILE__, __LINE__, THINKING, __VA_ARGS__); return T;
+#endif
+
+// print the current code pos + custom fmt + string into the kernel log and return(debug)
+#ifndef PRINT_RETURN
+#define PRINT_RETURN(fmt, ...) xUtilty::KernelPrintOut("%s %s %d %s "  fmt, __FUNCTION__, __FILE__, __LINE__, THINKING, __VA_ARGS__); return;
+#define PRINT_RETURN_N(fmt, ...) xUtilty::Notify("%s %s %d %s "  fmt, __FUNCTION__, __FILE__, __LINE__, THINKING, __VA_ARGS__); return;
+
+#endif
+
+// print the current code pos + custom string into the kernel log and return(debug)
+#ifndef PRINT_STRING_RETURN 
+#define PRINT_STRING_RETURN(string)  xUtilty::KernelPrintOut("%s %s %d %s", __FUNCTION__, __FILE__, __LINE__, string); return;
+#define PRINT_STRING_RETURN_N(string)  xUtilty::Notify("%s %s %d %s", __FUNCTION__, __FILE__, __LINE__, string); return;
+#endif
+
+// print the current code pos + custom string into the kernel log(debug)
+#ifndef PRINT_STRING
+#define PRINT_STRING(string)  xUtilty::KernelPrintOut("%s %s %d %s", __FUNCTION__,__FILE__, __LINE__, string);
+#define PRINT_STRING_N(string)  xUtilty::Notify("%s %s %d %s", __FUNCTION__,__FILE__, __LINE__, string);
+#endif
+
+namespace xUtilty
+{
+	enum MessageType
+	{
+		Local,
+		Kernel,
+		Visual
+	};
+
+	enum NotifyType
+	{
+		NotificationRequest = 0,
+		SystemNotification = 1,
+		SystemNotificationWithUserId = 2,
+		SystemNotificationWithDeviceId = 3,
+		SystemNotificationWithDeviceIdRelatedToUser = 4,
+		SystemNotificationWithText = 5,
+		SystemNotificationWithTextRelatedToUser = 6,
+		SystemNotificationWithErrorCode = 7,
+		SystemNotificationWithAppId = 8,
+		SystemNotificationWithAppName = 9,
+		SystemNotificationWithAppInfo = 9,
+		SystemNotificationWithAppNameRelatedToUser = 10,
+		SystemNotificationWithParams = 11,
+		SendSystemNotificationWithUserName = 12,
+		SystemNotificationWithUserNameInfo = 13,
+		SendAddressingSystemNotification = 14,
+		AddressingSystemNotificationWithDeviceId = 15,
+		AddressingSystemNotificationWithUserName = 16,
+		AddressingSystemNotificationWithUserId = 17,
+		UNK_1 = 100,
+		TrcCheckNotificationRequest = 101,
+		NpDebugNotificationRequest = 102,
+		UNK_2 = 102,
+	};
+
+	struct NotifyBuffer
+	{
+		NotifyType Type;
+		int ReqId;
+		int Priority;
+		int MsgId;
+		int TargetId;
+		int UserId;
+		int unk1;
+		int unk2;
+		int AppId;
+		int ErrorNum;
+		int unk3;
+		char UseIconImageUri;
+		char Message[1024];
+		char Uri[1024];
+		char unkstr[1024];
+	};
+
+	// self contained vsprinf, if patches are applied this will do the same thing KernelPrintOut does.
+	void LocalPrint(const char* a_fmt, ...);
+
+	// Pushes a Notifcation and writes to the kernel log
+	void Notify(const char* a_fmt, ...);
+
+	// writes to the kernel log
+	void KernelPrintOut(const char* a_fmt, ...);
+
+	// Writes to the log using sceCoredumpDebugTextOut(), this has to be used instead of the others when in the coredump context otherwise you'll get a invaild syscall error.
+	void CoredumpPrintOut(const char* a_fmt, ...);
+
+	//Writes to Neighborhood Console Output
+	void PrintNeighborhood(const char* a_fmt, ...);
+}
